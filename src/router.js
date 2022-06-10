24c13de8a2049db623f1b9b1c1547611db7590b7
Gabriel Fernandes <fernandes.gh@outlook.com>
2022-06-10 12:32:24 -0300

    feat: route /create-user

--- src/router.js
+++ src/router.js
@@ -1,18 +1,8 @@
 import { Router } from 'express';
-import { Sequelize } from 'sequelize';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async(req, res) => {
-
-    const user = await User.create({
-        name: 'Gabriel',
-        email: 'gf@gmail.com',
-        password_hash: '123456',
-    });
-    console.log(user.toJSON());
-    return res.json(user);
-});
+routes.post('/create-user', UserController.store);
 
 export default routes;
